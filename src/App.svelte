9792e79f7606cc5c089800ec5a57cacf03dcc562
Vaishnav <binnunair71199@gmail.com>
2024-11-04 11:28:59 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Camera from "./camera.svelte";
+import FileDrop from "./fileDrop.svelte";
+</script>
+
+
+<main>
+	<FileDrop/>
+	<!-- <Camera /> -->
+</main>
+<button style="color: blue">
+	Submit for Processing
+</button>
+
+<style>
+	main {
+		background-color: grey;
+		text-align: center;	
+		display: block;
+		margin: auto;
+		padding: 10%;
+		padding-bottom: 25%;
+		max-width: 70%;
+		height: fit-content;
+	}
+	button {
+		display: block;
+		margin: auto;
+		margin-top: 5%;
+	}
+</style>
---
+++ src/fileDrop.svelte
@@ -0,0 +1,37 @@
+<script>
+    import App from "./App.svelte";
+
+    export const actions = {
+        login: async (event) => {
+            // TODO log the user in
+        },
+        register: async (event) => {
+            // TODO register the user
+        }
+    }
+    let filename = "";
+    let arr = []
+
+    // Function to handle file input change
+    function handleFileChange(event) {
+        const file = event.target.files[0];
+        if (file) {
+            filename = file.name;
+            arr = [...arr,filename]
+            console.log(arr)
+        }
+    }
+</script>
+
+<main>
+    <form method="POST" action="?/login">
+        <label>
+            <input name="email" type="file" on:change={handleFileChange} />
+            {#if filename}
+                {#each arr as file}
+                    <p> {file} </p>
+                {/each}
+            {/if}
+        </label>
+    </form>
+</main>
